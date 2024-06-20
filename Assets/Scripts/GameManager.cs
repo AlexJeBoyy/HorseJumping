f4917d990586a033722312609bfc58ad55863e40
AlexJeBoyy <a.strikwerda@sintlucasedu.nl>
2024-06-20 10:51:30 +0200

    changed bg

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,7 +13,7 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get; private set; }
     public float initialGameSpeed = 0f;
     public float lastSpeed;
-    public float gameSpeedIncrease = 0.1f;
+    public float gameSpeedIncrease = 0.2f;
     public float gameSpeed { get; private set; }    
     public bool firstSteps;
     
